99e01407a5cd2c9d6fb47d98258187f8b50677ee
Dana <danielashklover@gmail.com>
2018-10-09 13:07:53 -0700

    DataHolder more readable

--- src/DataHolder/DataHolder.java
+++ src/DataHolder/DataHolder.java
@@ -13,41 +13,41 @@ public class DataHolder {
         this.dataholder = dataholder;
     }
 
-    public void insertTimeObj(Calendar d, TimeObj t){
-        ArrayList al = dataholder.get(d);
-        al.add(t);
-        dataholder.put(d, al);
+    public void insertTimeObj(Calendar calendar, TimeObj timeObj){
+        ArrayList singleDayData = dataholder.get(calendar);
+        singleDayData.add(timeObj);
+        dataholder.put(calendar, singleDayData);
     }
 
-    public void deleteTimeObj(Calendar d, String search){
+    public void deleteTimeObj(Calendar calendar, String titleOfTimeObjToDelete){
         TimeObj edit = new TimeObj();
-        ArrayList<TimeObj> al = dataholder.get(d);
-        for (TimeObj t: al){
-            if (t.getTitle().equals(search)){
+        ArrayList<TimeObj> singleDayData = dataholder.get(calendar);
+        for (TimeObj t: singleDayData){
+            if (t.getTitle().equals(titleOfTimeObjToDelete)){
                 edit = t;
-                al.remove(t);
+                singleDayData.remove(t);
             }
         }
-        dataholder.put(d, al);
+        dataholder.put(calendar, singleDayData);
     }
 
-    public void changeTimeObj(Calendar d, Time stime, Time etime, String s, String search){
+    public void changeTimeObj(Calendar calendar, Time stime, Time etime, String note, String titleOfTimeObjtoDelete){
         TimeObj edit = new TimeObj();
-        ArrayList<TimeObj> al = dataholder.get(d);
-        for (TimeObj t: al){
-            if (t.getTitle().equals(search)){
+        ArrayList<TimeObj> singleDayData = dataholder.get(calendar);
+        for (TimeObj t: singleDayData){
+            if (t.getTitle().equals(titleOfTimeObjtoDelete)){
                edit = t;
-               al.remove(t);
+               singleDayData.remove(t);
             }
         }
         if (stime != null && etime != null){
             edit.setStart(stime);
             edit.setEnd(etime);
         }
-        if (s != null){
-            edit.setNote(s);
+        if (note != null){
+            edit.setNote(note);
         }
-        al.add(edit);
-        dataholder.put(d, al);
+        singleDayData.add(edit);
+        dataholder.put(calendar, singleDayData);
     }
 }
