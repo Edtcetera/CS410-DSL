26dde27847f52d9fdcccd9779195f86819fb9c66
Edward <edwardcai@outlook.com>
2018-10-09 13:53:37 -0700

    commit

--- src/ast/UPDATE.java
+++ src/ast/UPDATE.java
@@ -4,13 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 public class UPDATE extends STATEMENT {
+    String year;
     @Override
     public void parse() {
-
+        tokenizer.getAndCheckNext("SCHEDULE");
+        year = tokenizer.getNext();
     }
 
     @Override
     public String evaluate() throws FileNotFoundException, UnsupportedEncodingException {
+        //todo: place year into DataHolder to see year currently working on
         return null;
     }
 }
