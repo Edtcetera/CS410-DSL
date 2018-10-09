29e08aa1bc32dfccfd48bc371d514b3667dbd0dc
Dana <danielashklover@gmail.com>
2018-10-09 13:28:32 -0700

    Dataholder turned Singleton

--- src/DataHolder/DataHolder.java
+++ src/DataHolder/DataHolder.java
@@ -5,35 +5,44 @@ import TimeObj.TimeObj;
 import java.sql.Time;
 import java.util.*;
 
+
+// Singleton implemented : Probably not amazingly. TODOTODO
+
 public class DataHolder {
-    Map <Calendar, ArrayList<TimeObj>> dataholder = new HashMap();
+    Map <Calendar, ArrayList<TimeObj>> dh = new HashMap();
+    private static DataHolder dataHolder = null;
 
-
-    public void DataHolder(Map dataholder){
-        this.dataholder = dataholder;
+    private DataHolder(){
     }
+    public static DataHolder getInstance(){
+
+            if (dataHolder == null)
+                dataHolder = new DataHolder();
+
+            return dataHolder;
+        }
 
     public void insertTimeObj(Calendar calendar, TimeObj timeObj){
-        ArrayList singleDayData = dataholder.get(calendar);
+        ArrayList singleDayData = dh.get(calendar);
         singleDayData.add(timeObj);
-        dataholder.put(calendar, singleDayData);
+        dh.put(calendar, singleDayData);
     }
 
     public void deleteTimeObj(Calendar calendar, String titleOfTimeObjToDelete){
         TimeObj edit = new TimeObj();
-        ArrayList<TimeObj> singleDayData = dataholder.get(calendar);
+        ArrayList<TimeObj> singleDayData = dh.get(calendar);
         for (TimeObj t: singleDayData){
             if (t.getTitle().equals(titleOfTimeObjToDelete)){
                 edit = t;
                 singleDayData.remove(t);
             }
         }
-        dataholder.put(calendar, singleDayData);
+        dh.put(calendar, singleDayData);
     }
 
     public void changeTimeObj(Calendar calendar, Time stime, Time etime, String note, String titleOfTimeObjtoDelete){
         TimeObj edit = new TimeObj();
-        ArrayList<TimeObj> singleDayData = dataholder.get(calendar);
+        ArrayList<TimeObj> singleDayData = dh.get(calendar);
         for (TimeObj t: singleDayData){
             if (t.getTitle().equals(titleOfTimeObjtoDelete)){
                edit = t;
@@ -48,6 +57,6 @@ public class DataHolder {
             edit.setNote(note);
         }
         singleDayData.add(edit);
-        dataholder.put(calendar, singleDayData);
+        dh.put(calendar, singleDayData);
     }
 }
